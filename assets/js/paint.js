185f9baeec2e497b128f3a78a50b3144639c0c86
Stino89 <ftr1989@gmail.com>
2024-01-28 10:48:14 -0800

    removed 7 errors

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,52 +1,44 @@
 function configureListeners() {
-    var images = document.getElementsByTagName('img');// select img elements  
+    var images = document.getElementsByTagName('img');    
 
-
-     for (var i = 0; i < images.length; i++) { 
-        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)       
-        document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false)// iterate over images and add mouseover event listeners      
+     for (var i = 0; i < images.length; i++) {        
+        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)        
+        document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false)        
     } 
 }
 
 function addOpacity(event) {
     if (!this.classList.contains('dim')){
         this.classList.add('dim')
-    } 
-    // add appropriate CSS class
+    }    
     getProductInfo(event.target.id);     
 }
 
 function removeOpacity(event) {
-    if (this.classList.contains('dim')){
+     if (this.classList.contains('dim')){
         this.classList.remove('dim');
     }
-     //remove appropriate CSS class
 
-    let element = document.getElementById('ppg');
     let element = document.getElementById('color-price');
         element.textContent = '';
         
-    let color = document.getElementById('color');
     let color = document.getElementById('color-name');
         color.textContent = ''; 
 
     event.preventDefault();    
 }
-function changeImage(elementId) {
-    let image = document.getElementById('imgDisplay');
-    image.src = elementId.src;
-}
+
 // function changeImage(elementId) {
 //     let image = document.getElementById('imgDisplay');
 //     image.src = elementId.src;
 // }
 
-function getProductInfo(part number) {
+function getProductInfo(partNumber) {
     let price;
     let colorName;  
     
     switch (partNumber) {
-        case 'pn1':
+        case 'pn1':           
             price = '$19.99'
             colorName = 'Lime Green'
             updatePrice(colorName, price)           
@@ -54,7 +46,7 @@ function getProductInfo(part number) {
         case 'pn2':
             price = '$12.99'
             colorName = 'Medium Brown' 
-            updatePrice(colorName, price)
+            updatePrice(colorName, price)     
             break;            
         case 'pn3':
             price = '$11.99'
@@ -64,13 +56,13 @@ function getProductInfo(part number) {
         case 'pn4':
             price = '$14.99'
             colorName = 'Bright Red'            
-            updatePrice(colorName, price)    
+            updatePrice(colorName, price)   
             break;   
         case 'pn5':
             price = '$9.99'
             colorName = 'Solid White' 
-            updatePrice(colorName, price) 
-            break;
+            updatePrice(colorName, price)              
+            break;   
         case 'pn6':
             price = '$15.99'
             colorName = 'Solid Black'   
@@ -79,28 +71,28 @@ function getProductInfo(part number) {
         case 'pn7':
             price = '$8.99'
             colorName = 'Medium Blue'       
-            updatePrice(colorName, price)           
+            updatePrice(colorName, price)        
             break;   
         case 'pn8':
             price = '$16.99'
             colorName = 'Light Purple'      
-            updatePrice(colorName, price)             
+            updatePrice(colorName, price)         
             break;   
         case 'pn9':
             price = '$17.99'
             colorName = 'Bright Yellow'                       
-            updatePrice(colorName, price)          
+            updatePrice(colorName, price)   
             break;   
           default:              
     }
 
     function updatePrice(colorName, price)
     {       
-        let colorPrice = // select element with corresponding id
-        // display price
+        let colorPrice = document.getElementById('color-price');
+        colorPrice.textContent = price;
         
-        let color = // select element with corresponding id
-        //display color name
+        let color = document.getElementById('color-name');
+        color.textContent = colorName;
     }
     
 }
