fbd59ce585355b342152b48c5bfdf0f04f1502cb
Stino89 <ftr1989@gmail.com>
2024-01-28 10:18:53 -0800

    line 25 &26

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,20 +1,28 @@
 function configureListeners() {
-    let images = // select img elements  
+    var images = document.getElementsByTagName('img');// select img elements  
 
 
-     for (var i = 0; i < images.length; i++) {        
-        // iterate over images and add mouseover event listeners      
+     for (var i = 0; i < images.length; i++) { 
+        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)       
+        document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false)// iterate over images and add mouseover event listeners      
     } 
 }
 
 function addOpacity(event) {
+    if (!this.classList.contains('dim')){
+        this.classList.add('dim')
+    } 
     // add appropriate CSS class
     getProductInfo(event.target.id);     
 }
 
 function removeOpacity(event) {
+    if (this.classList.contains('dim')){
+        this.classList.remove('dim');
+    }
      //remove appropriate CSS class
 
+    let element = document.getElementById('ppg');
     let element = document.getElementById('color-price');
         element.textContent = '';
         
